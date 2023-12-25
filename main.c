17fec074fb390aa28a1c16401a93067779116f29
Gabriel Passos <gabriel80546@gmail.com>
2023-12-25 20:44:30 -0300

    ./push.sh

--- main.c
+++ main.c
@@ -1,10 +1,62 @@
 #include <stdio.h>
 
 void showBoard(int* board12and10) {
-    int i, j;
+    int i, j, k;
     for(j = 0; j < 8; j++) {
         for(i = 0; i < 8; i++) {
-            printf("%i ", board12and10[21 + i + (j * 10)]);
+            k = board12and10[21 + i + (j * 10)];
+            switch (k) {
+            case 0x00:
+                printf("_ ");
+                break;
+
+            case 0x01:
+                printf("p ");
+                break;
+            case 0x81:
+                printf("P ");
+                break;
+
+            case 0x02:
+                printf("n ");
+                break;
+            case 0x82:
+                printf("N ");
+                break;
+
+            case 0x03:
+                printf("b ");
+                break;
+            case 0x83:
+                printf("B ");
+                break;
+
+            case 0x04:
+                printf("r ");
+                break;
+            case 0x84:
+                printf("R ");
+                break;
+
+            case 0x05:
+                printf("q ");
+                break;
+            case 0x85:
+                printf("Q ");
+                break;
+
+            case 0x06:
+                printf("k ");
+                break;
+            case 0x86:
+                printf("K ");
+                break;
+
+
+            default:
+                printf("? ");
+                break;
+            }
         }
         printf("\n");
     }
@@ -13,18 +65,59 @@ void showBoard(int* board12and10) {
 
 int main() {
     int i;
-    int board[120] = {   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,
-                        11,  12,  13,  14,  15,  16,  17,  18,  19,  20,
-                        21,  22,  23,  24,  25,  26,  27,  28,  29,  30,
-                        31,  32,  33,  34,  35,  36,  37,  38,  39,  40,
-                        41,  42,  43,  44,  45,  46,  47,  48,  49,  50,
-                        51,  52,  53,  54,  55,  56,  57,  58,  59,  60,
-                        61,  62,  63,  64,  65,  66,  67,  68,  69,  70,
-                        71,  72,  73,  74,  75,  76,  77,  78,  79,  80,
-                        81,  82,  83,  84,  85,  86,  87,  88,  89,  90,
-                        91,  92,  93,  94,  95,  96,  97,  98,  99, 100,
-                       101, 102, 103, 104, 105, 106, 107, 108, 109, 110,
-                       111, 112, 113, 114, 115, 116, 117, 118, 119, 120 };
+    int board[120] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+                       0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+                       0xFF, 0x04, 0x02, 0x03, 0x05, 0x06, 0x03, 0x02, 0x04, 0xFF,
+                       0xFF, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0xFF,
+                       0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF,
+                       0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF,
+                       0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF,
+                       0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF,
+                       0xFF, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0xFF,
+                       0xFF, 0x84, 0x82, 0x83, 0x85, 0x86, 0x83, 0x82, 0x84, 0xFF,
+                       0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+                       0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
     showBoard(board);
     return 0;
 }
+
+/*
+; *******************************************************
+; BOARD	-- Board Array. Used to hold the current position 
+; of the board during play. The board itself
+; looks like: 
+; 	FFFFFFFFFFFFFFFFFFFF 
+; 	FFFFFFFFFFFFFFFFFFFF 
+; 	FF0402030506030204FF 
+; 	FF0101010101010101FF 
+; 	FF0000000000000000FF 
+; 	FF0000000000000000FF 
+; 	FF0000000000000060FF 
+; 	FF0000000000000000FF 
+; 	FF8181818181818181FF 
+; 	FF8482838586838284FF 
+; 	FFFFFFFFFFFFFFFFFFFF 
+; 	FFFFFFFFFFFFFFFFFFFF 
+; The values of FF form the border of the 
+; board, and are used to indicate when a piece 
+; moves off the board. The individual bits of 
+; the other bytes in the board array are as
+; follows:
+; 	Bit 7 -- Color of the piece
+; 	1 -- Black 
+; 	0 -- White 
+; 	Bit 6 -- Not used 
+; 	Bit 5 -- Not used 
+; 	Bit 4 --Castle flag for Kings only
+; 	Bit 3 -- Piece has moved flag
+; 	Bits 2-0 Piece type 
+; 		1 -- Pawn 
+; 		2 -- Knight
+; 		3 -- Bishop 
+; 		4 -- Rook 
+; 		5 -- Queen 
+; 		6 -- King
+; 		7 -- Not used
+; 		0 -- Empty Square
+; *******************************************************
+*/
